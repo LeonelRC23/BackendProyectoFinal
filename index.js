c331e7ed0998666d28b492216401dafe27222e8a
Leonel <leonelrcordero22@gmail.com>
2024-03-26 18:40:53 -0300

    Se configuraron los middlewares requeridos y el puerto a utilizar

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import morgan from 'morgan';
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), () => {
+  console.log('Puerto utilizado: ' + app.get('port'));
+});
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
